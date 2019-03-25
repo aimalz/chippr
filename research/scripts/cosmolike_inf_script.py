3a6ad762699d35416d41a8e59d71a95e3a956338
Alex Malz <aimalz@nyu.edu>
2019-03-25 14:31:25 -0400

    want to run the cosmolike tests on broiler

---
+++ research/scripts/cosmolike_inf_script.py
@@ -0,0 +1,203 @@
+def check_prob_params(params):
+    """
+    Sets parameter values pertaining to components of probability
+
+    Parameters
+    ----------
+    params: dict
+        dictionary containing key/value pairs for probability
+
+    Returns
+    -------
+    params: dict
+        dictionary containing key/value pairs for probability
+    """
+    if 'prior_mean' not in params:
+        params['prior_mean'] = 'interim'
+    else:
+        params['prior_mean'] = params['prior_mean'][0]
+    if 'no_prior' not in params:
+        params['no_prior'] = 0
+    else:
+        params['no_prior'] = int(params['no_prior'][0])
+    if 'no_data' not in params:
+        params['no_data'] = 0
+    else:
+        params['no_data'] = int(params['no_data'][0])
+    return params
+
+# def make_true_nz(test_name):
+#     """
+#     Function to create true redshift distribution to be shared among several
+#     test cases
+#
+#     Parameters
+#     ----------
+#     test_name: string
+#         name used to look up parameters for making true_nz
+#
+#     Returns
+#     -------
+#     true_nz: chippr.gmix object
+#         gaussian mixture probability distribution
+#
+#     Notes
+#     -----
+#     test_name is currently ignored but will soon be used to load parameters for making true_nz instead of hardcoded values.
+#     """
+#     true_amps = np.array([0.20, 0.35, 0.55])
+#     true_means = np.array([0.5, 0.2, 0.75])
+#     true_sigmas = np.array([0.4, 0.2, 0.1])
+#
+#     true_nz = chippr.gmix(true_amps, true_means, true_sigmas, limits=(0., 1.))
+#
+#     return true_nz
+
+def set_up_prior(data, params):
+    """
+    Function to create prior distribution from data
+
+    Parameters
+    ----------
+    data: dict
+        catalog dictionary containing bin endpoints, log interim prior, and log
+        interim posteriors
+    params: dict
+        dictionary of parameter values for creation of prior
+
+    Returns
+    -------
+    prior: chippr.mvn object
+        prior distribution as multivariate normal
+    """
+    zs = data['bin_ends']
+    log_nz_intp = data['log_interim_prior']
+    log_z_posts = data['log_interim_posteriors']
+
+    z_difs = zs[1:]-zs[:-1]
+    z_mids = (zs[1:]+zs[:-1])/2.
+    n_bins = len(z_mids)
+
+    n_pdfs = len(log_z_posts)
+
+    a = 1.# / n_bins
+    b = 3.#1. / z_difs ** 2
+    c = 3.e-2#a / n_pdfs
+    prior_var = np.eye(n_bins)
+    for k in range(n_bins):
+        prior_var[k] = a * np.exp(-0.5 * b * (z_mids[k] - z_mids) ** 2)
+    prior_var += c * np.identity(n_bins)
+
+    prior_mean = log_nz_intp
+    prior = mvn(prior_mean, prior_var)
+    if params['prior_mean'] == 'sample':
+        new_mean = prior.sample_one()
+        prior = mvn(new_mean, prior_var)
+        print(params['prior_mean'], prior_mean, new_mean)
+    else:
+        print(params['prior_mean'], prior_mean)
+
+    return (prior, prior_var)
+
+def do_inference(given_key):
+    """
+    Function to do inference from a catalog of photo-z interim posteriors
+
+    Parameters
+    ----------
+    given_key: string
+        name of test case to be run
+    """
+    test_info = all_tests[given_key]
+    test_name = test_info['name']
+
+    test_name = test_name[:-1]
+    param_file_name = test_name + '.txt'
+
+    params = chippr.utils.ingest(param_file_name)
+    params = check_prob_params(params)
+    params = defaults.check_inf_params(params)
+    print(params)
+
+    test_dir = os.path.join(result_dir, test_name)
+    simulated_posteriors = catalog(params=param_file_name, loc=test_dir, prepend=test_name)
+    saved_location = 'data'
+    saved_type = '.txt'
+    data = simulated_posteriors.read(loc=saved_location, style=saved_type)
+    zs = data['bin_ends']
+    z_difs = zs[1:]-zs[:-1]
+    with open(os.path.join(os.path.join(test_dir, saved_location), 'true_vals.txt'), 'r') as true_file:
+        true_data = csv.reader(true_file, delimiter=' ')
+        true_vals = []
+        for z in true_data:
+            true_vals.append(float(z[0]))
+        true_vals = np.array(true_vals)
+        true_vals = np.histogram(true_vals, bins=zs, normed=True)[0]
+    true_nz = chippr.discrete(zs, true_vals)
+
+    (prior, cov) = set_up_prior(data, params)
+
+    nz = log_z_dens(data, prior, truth=true_nz, loc=test_dir, prepend=test_name, vb=True)
+
+    nz_stacked = nz.calculate_stacked()
+    # print('stacked: '+str(np.dot(np.exp(nz_stacked), z_difs)))
+    nz_mmap = nz.calculate_mmap()
+    # print('MMAP: '+str(np.dot(np.exp(nz_mmap), z_difs)))
+    # nz_mexp = nz.calculate_mexp()
+    # print('MExp: '+str(np.dot(np.exp(nz_mexp), z_difs)))
+
+    start_mmle = timeit.default_timer()
+    nz_mmle = nz.calculate_mmle(nz_stacked, no_data=params['no_data'], no_prior=params['no_prior'])
+    end_mmle = timeit.default_timer()-start_mmle
+    print('MMLE: '+str(np.dot(np.exp(nz_mmle), z_difs))+' in '+str(end_mmle))
+
+    nz_stats = nz.compare()
+    nz.plot_estimators(log=True, mini=False)
+    nz.plot_estimators(log=False, mini=False)
+    nz.write('nz.p')
+
+    # COMMENT OUT TO AVOID SAMPLING
+    #start_mean = mvn(nz_mmle, cov).sample_one()
+    start = prior#mvn(data['log_interim_prior'], cov)
+
+    n_bins = len(zs) - 1
+    if params['n_walkers'] is not None:
+        n_ivals = params['n_walkers']
+    else:
+        n_ivals = 10 * n_bins
+    initial_values = start.sample(n_ivals)
+
+    start_samps = timeit.default_timer()
+    nz_samps = nz.calculate_samples(initial_values, no_data=params['no_data'], no_prior=params['no_prior'], n_procs=1)
+    time_samps = timeit.default_timer()-start_samps
+    print('Sampled '+str(params['n_accepted'])+' after '+str(nz.burn_ins * params['n_burned'])+' in '+str(time_samps))
+
+    nz_stats = nz.compare()
+    nz.plot_estimators(log=True, mini=False)
+    nz.plot_estimators(log=False, mini=False)
+    nz.write('nz.p')
+
+if __name__ == "__main__":
+
+    import numpy as np
+    import pickle
+    import os
+    import multiprocessing as mp
+
+    import chippr
+    from chippr import *
+
+    result_dir = os.path.join('..', 'results')
+    # name_file = 'which_inf_tests.txt'
+    tests_to_run = ['0single_lsst ', '1single_lsst ', '2single_lsst ', '3single_lsst ']
+
+    # with open(name_file) as tests_to_run:
+    all_tests = {}
+    for test_name in tests_to_run:
+        test_info = {}
+        test_info['name'] = test_name
+        all_tests[test_name] = test_info
+
+    nps = mp.cpu_count()
+    pool = mp.Pool(nps)
+    pool.map(do_inference, all_tests.keys())
---
+++ research/scripts/cosmolike_sim_script.py
@@ -0,0 +1,247 @@
+def check_extra_params(params):
+    """
+    Sets parameter values pertaining to true n(z)
+
+    Parameters
+    ----------
+    params: dict
+        dictionary containing key/value pairs for simulation
+
+    Returns
+    -------
+    params: dict
+        dictionary containing key/value pairs for simulation
+    """
+    # if 'smooth_truth' not in params:
+    #     params['smooth_truth'] = 0
+    # else:
+    #     params['smooth_truth'] = int(params['smooth_truth'][0])
+    #     # params['true_shape'] = int(params['true_shape'][0])
+    #     params['true_loc'] = float(params['true_loc'][0])
+    #     params['true_scale'] = 1./float(params['true_scale'][0])
+
+    if 'interim_prior' not in params:
+        params['interim_prior'] = 'flat'
+    else:
+        params['interim_prior'] = str(params['interim_prior'][0])
+    if 'wrong_prior' not in params:
+        params['wrong_prior'] = 0
+    else:
+        params['wrong_prior'] = int(params['wrong_prior'][0])
+        if 'bias_model' not in params:
+            params['bias_model'] = 'flat'
+        else:
+            params['bias_model'] = str(params['bias_model'][0])
+    if 'n_galaxies' not in params:
+        params['n_galaxies'] = 10**4
+    else:
+        params['n_galaxies'] = 10**int(params['n_galaxies'][0])
+
+    return(params)
+
+def read_cosmolike(bin_ends, loc='.', fn='nz_histo.txt'):
+    """
+    Turns cosmolike n(z) file into true n(z)s
+
+    Parameters
+    ----------
+    given_key: string
+        name of test case for which true n(z) is to be made
+    loc: string
+        path to file with n(z) evaluations
+    fn: string
+        filename
+
+    Returns
+    -------
+    true_nzs: list, chippr.discrete objects
+        per-bin true_nzs
+    """
+    bin_mids = (bin_ends[1:] + bin_ends[:-1]) / 2.
+    cl_input = np.genfromtxt(loc+'/'+fn).T
+    n_tomobins = len(cl_input) - 1
+    zs = cl_input[0]
+    nzs = cl_input[1:]
+    true_nzs = []
+    writeout = []
+    for b in range(n_tomobins):
+        f = spi.interp1d(zs, nzs[b])
+        bin_vals = f(bin_mids)
+        writeout.append(bin_vals)
+        true_func = discrete(bin_ends, bin_vals)
+        true_nzs.append(true_func)
+
+    cl_data = np.empty((n_tomobins, len(bin_mids)))
+    cl_data[0] = bin_mids
+    for i in range(n_tomobins - 1):
+        cl_data[i+1] = writeout[i]
+    np.savetxt(loc+'/'+'downbinned_'+fn, cl_data)
+
+    return(true_nzs)
+
+def make_interim_prior(given_key, wrong=False, grid=None):
+    """
+    Function to make the histogram-parametrized interim prior
+
+    Parameters
+    ----------
+    given_key: string
+        name of test case for which interim prior is to be made
+    wrong: Boolean, optional
+        wrongify the interim prior?
+    grid: numpy.ndarray, float, optional
+        grid for interim prior
+
+    Returns
+    -------
+    interim_prior: chippr.discrete or chippr.gauss or chippr.gmix object
+        the discrete distribution that will be the interim prior
+    """
+    test_info = all_tests[given_key]
+    bin_mids = (test_info['bin_ends'][1:] + test_info['bin_ends'][:-1]) / 2.
+    bin_difs = test_info['bin_ends'][1:] - test_info['bin_ends'][:-1]
+
+    if not wrong:
+        int_pr_type = test_info['params']['interim_prior']
+        print('using true interim prior of '+int_pr_type)
+    else:
+        int_pr_type = test_info['params']['bias_model']
+        print('using wrong interim prior of '+int_pr_type)
+
+    if int_pr_type == 'template':
+        bin_range = max(test_info['bin_ends']) - min(test_info['bin_ends'])
+        int_amps = np.array([0.35, 0.5, 0.15])
+        int_means = np.array([0.1, 0.5, 0.9]) * bin_range + min(test_info['bin_ends'])
+        int_sigmas = np.array([0.1, 0.1, 0.1]) * bin_range
+        n_mix_comps = len(int_amps)
+        int_funcs = []
+        for c in range(n_mix_comps):
+            int_funcs.append(chippr.gauss(int_means[c], int_sigmas[c]**2))
+        interim_prior = chippr.gmix(int_amps, int_funcs,
+            limits=(min(test_info['bin_ends']), max(test_info['bin_ends'])))
+    elif int_pr_type == 'training':
+        int_amps = [0.150,0.822,1.837,2.815,3.909,
+                              5.116,6.065,6.477,6.834,7.304,
+                              7.068,6.771,6.587,6.089,5.165,
+                              4.729,4.228,3.664,3.078,2.604,
+                              2.130,1.683,1.348,0.977,0.703,
+                              0.521,0.339,0.283,0.187,0.141,
+                              0.104,0.081,0.055,0.043,0.034]
+        int_grid = np.linspace(0., 1.1, len(int_amps) + 1)
+        # int_grid = np.concatenate((int_grid, np.array([test_info['bin_ends'][-1]])))
+        # print(int_grid)
+        int_amps.append(int_amps[-1])
+        int_amps = np.array(int_amps)
+        # this basically hardcodes in that grids start at 0.!
+
+        int_grid_mids = (int_grid[1:] + int_grid[:-1]) / 2.
+
+        int_grid_mids = np.concatenate((int_grid_mids, np.array([test_info['bin_ends'][-1]])))
+        f = spi.interp1d(int_grid_mids, int_amps)
+        int_amps = f(bin_mids)
+        int_means = bin_mids
+        int_sigmas = bin_difs
+        n_mix_comps = len(int_amps)
+        int_funcs = []
+        for c in range(n_mix_comps):
+            int_funcs.append(chippr.gauss(int_means[c], int_sigmas[c]**2))
+        interim_prior = chippr.gmix(int_amps, int_funcs,
+                limits=(min(test_info['bin_ends']), max(test_info['bin_ends'])))
+    else:
+        bin_ends = test_info['bin_ends']
+        weights = np.ones_like(bin_mids)
+        interim_prior = chippr.discrete(bin_ends, weights)
+
+    return(interim_prior)
+
+def make_catalog_pre_split(given_key):
+    """
+    Function to create a catalog once true redshifts exist
+
+    Parameters
+    ----------
+    given_key: string
+        name of test case to be run
+    """
+    test_info = all_tests[given_key]
+    test_name = test_info['name']
+
+    param_file_name = test_name + '.txt'
+    params = chippr.utils.ingest(param_file_name)
+    params = defaults.check_sim_params(params)
+    params = check_extra_params(params)
+    test_info['params'] = params
+
+    test_info['bin_ends'] = np.linspace(test_info['params']['bin_min'],
+                                test_info['params']['bin_max'],
+                                test_info['params']['n_bins']+1)
+    # print('bin ends to simulation = '+str(test_info['bin_ends']))
+
+    # replace make_true with read_cosmolike in loop
+    # test_info, true_nz = make_true(given_key)
+    true_nzs = read_cosmolike(test_info['bin_ends'], loc='../../../CosmoLike/chippr_forecasts', fn='nz_histo.txt')
+
+    interim_prior = make_interim_prior(given_key)
+
+    return(test_info, interim_prior, true_nzs, param_file_name)
+
+def make_catalog_at_split(given_key, param_file_name, interim_prior, true_nz):
+
+    test_info['dir'] = test_dir
+    if os.path.exists(test_dir):
+        shutil.rmtree(test_dir)
+    os.makedirs(test_dir)
+
+    posteriors = chippr.catalog(param_file_name, loc=test_dir, prepend=test_name)
+    output = posteriors.create(true_nz, interim_prior, N=test_info['params']['n_gals'])
+    print('using implicit prior '+str(posteriors.cat['log_interim_prior']))
+    # data = np.exp(output['log_interim_posteriors'])
+    # print('bin ends from simulation: '+str(posteriors.bin_ends))
+
+    if test_info['params']['wrong_prior']:
+        int_pr = make_interim_prior(given_key, wrong=True)
+        int_pr_fine = np.array([int_pr.pdf(posteriors.z_fine)])
+        int_pr_coarse = posteriors.coarsify(int_pr_fine)
+        posteriors.cat['log_interim_prior'] = u.safe_log(int_pr_coarse[0])
+    print('writing implicit prior '+str(posteriors.cat['log_interim_prior']))
+
+    posteriors.write()
+    test_info['truth'] = {'bin_ends': true_nz.bin_ends, 'weights': true_nz.distweights}
+    data_dir = posteriors.data_dir
+    with open(os.path.join(data_dir, 'true_params.p'), 'w') as true_file:
+        pickle.dump(test_info['truth'], true_file)
+
+if __name__ == "__main__":
+
+    import numpy as np
+    import scipy.stats as sps
+    import pickle
+    import shutil
+    from shutil import copyfile
+    import os
+    import scipy.interpolate as spi
+    import timeit
+
+    import chippr
+    from chippr import *
+
+    result_dir = os.path.join('..', 'results')
+    test_name = 'single_lsst'
+
+    param_file_name = test_name + '.txt'
+
+    all_tests = {}
+    test_info = {}
+    test_info['name'] = test_name
+    all_tests[test_name] = test_info
+
+    test_info, imp_pr, true_nzs, orig_param_file_name = make_catalog_pre_split(test_name)
+
+    for i in range(len(true_nzs)):
+        param_file_name = str(i) + orig_param_file_name
+        copyfile(orig_param_file_name, param_file_name)
+
+        new_test_name = str(i) + test_name
+        test_dir = os.path.join(result_dir, new_test_name)
+
+        make_catalog_at_split(test_name, param_file_name, imp_pr, true_nzs[i])
