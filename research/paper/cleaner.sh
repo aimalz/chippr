31f63df62bb3bb42a3ed0e36bf5e6d3c5e555155
Alex Malz <aimalz@nyu.edu>
2019-12-13 12:06:52 -0500

    clearing output

--- research/paper/cleaner.sh
+++ research/paper/cleaner.sh
@@ -1,7 +1,7 @@
-infile='from_thesis.tex'
-outfile='draft.tex'
+#infile='thesis.tex'
+#outfile='draft.tex'
 
-sed -e "s/{\\textbackslash}'\\\\{i\\\\}/\\'{i}/g" from_thesis.bib | grep -E -v '^\W*(doi|url|urldate|abstract|file|keywords|annote|note)' > from_thesis.bib.tmp
-mv from_thesis.bib.tmp thesis.bib
+sed -e "s/{\\textbackslash}'\\\\{i\\\\}/\\'{i}/g" thesis.bib | grep -E -v '^\W*(doi|url|urldate|abstract|file|keywords|annote|note)' > thesis.bib.tmp
+mv thesis.bib.tmp thesis.bib
 
 #grep -E -v '^(%|[[:blank:]]*%|\\COMMENT|\includegraphics*)' "$infile" | fold -w80 -s > "$outfile"
